c0e814ab29a7b99956c4965b300449a27021cc62
jlee-ClassAround <jlee@classaround.co.kr>
2025-12-11 15:57:21 +0900

    vercel 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "utfs.io",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'utfs.io',
+            },
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
+
+    experimental: {
+        serverComponentsExternalPackages: ['@prisma/client'],
+        outputFileTracingIncludes: {
+            '/**': ['./src/generated/**'],
+        },
+    },
+
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals.push({
+                '@prisma/classaround-client': 'commonjs @prisma/classaround-client',
+                '@prisma/cojooboo-client': 'commonjs @prisma/cojooboo-client',
+                '@prisma/ivy-client': 'commonjs @prisma/ivy-client',
+            });
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
